0eb64d851482a57cabe21ecc59cabd564cb13cfa
monikakatta <kattamonika26@gmail.com>
2020-03-07 20:54:21 +0530

    first commit

---
+++ epamweek6/src/main/java/com/epam/epamweek6/App.java
@@ -0,0 +1,33 @@
+package com.epam.epamweek6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	CustomList<Integer> list = new CustomList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+       System.out.println(list);
+       System.out.println(list.size());
+       
+         list.add(11);
+         System.out.println(list.size());
+        list.remove(2);
+        System.out.println(list);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.size());
+    }
+}
